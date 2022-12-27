0ab456fb1752239823323fad87c9b48913642d26
BoraOzcoban <bora.ozcoban@hotmail.com>
2022-12-27 09:09:08 +0300

    comments

--- lib/Screens/comments.dart
+++ lib/Screens/comments.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Comments extends StatelessWidget {
   static const routeName = '/comment';
+
   const Comments({Key? key}) : super(key: key);
 
   @override
@@ -13,12 +14,48 @@ class Comments extends StatelessWidget {
         title: Text("Comments"),
       ),
       body: Container(
-        color: Colors.black38,
+        padding: EdgeInsets.all(20),
+        color: Colors.black12,
         child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Text("Comments"),
+          child: ListView(
+            children: [
+              SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 40),
+                child: Text(
+                  "Make a Comment: ",
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                  Icons.comment,
+                  color: Colors.black,
+                )),
+              ),
+              SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+                child: Text(
+                  "Comments: ",
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: [
+                        Column(
+                          children: [
+                            Text('Name Placeholder'),
+                          ],
+                        ),
+                    SizedBox(height: 50),
+                    Text('Comment Placeholder', style: TextStyle(fontWeight: FontWeight.w300),),
+                  ],
+                ),
+              ),
+
             ],
           ),
         ),
