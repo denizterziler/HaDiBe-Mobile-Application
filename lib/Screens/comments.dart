3be5eb22a81d6063da062654ba98603d58e6737d
BoraOzcoban <bora.ozcoban@hotmail.com>
2023-01-16 22:08:56 +0300

    New Comments

--- lib/Screens/comments.dart
+++ lib/Screens/comments.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:se_380_project/Models/content.dart';
 
 class Like {
   bool isLiked;
@@ -7,15 +10,38 @@ class Like {
 }
 
 class Comments extends StatefulWidget {
-  static const routeName = '/comment';
+  static const routeName = '/comments';
+  final String commentText;
+  final int commentId;
+  final String authorComment;
+  final String name;
 
-  const Comments({Key? key}) : super(key: key);
+
+  const Comments({Key? key, this.commentText ="",  this.commentId = 0, this.authorComment = "", this.name = ""}) : super(key: key);
 
   @override
   State<Comments> createState() => _CommentsState();
 }
 
 class _CommentsState extends State<Comments>{
+  String _username = '';
+  String _commentText = '';
+  int _commentId = 0;
+  String _authorComment = '';
+  String _name = '';
+
+  _fetch() async {
+    final firebaseUser = FirebaseAuth.instance.currentUser!;
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(firebaseUser.uid)
+        .get()
+        .then((value) {
+      _username = value.get('userName');
+    });
+  }
+
+
   List<String> commentArray = [];
   TextEditingController _controller = TextEditingController();
   List<Like> likes = [];
@@ -23,17 +49,38 @@ class _CommentsState extends State<Comments>{
   @override
   void initState() {
     super.initState();
+    _commentText = widget.commentText ?? '';
+    _commentId = widget.commentId ?? 0;
+    _authorComment = widget.authorComment ?? '';
+    _name = '';
   }
 
   void addComment() {
     setState(() {
       commentArray.add(_controller.text);
       likes.add(Like(isLiked: false));
+      saveComments(context,commentText: _controller.text, commentId: commentArray.length -1, authorComment: _username, name: _name);
     });
+  }
+  void saveComments(BuildContext context,{required String commentText, required int commentId, required String authorComment, required String name}) async {
+    try {
+      await FirebaseFirestore.instance.collection("comments").doc(commentId.toString()).set({
+        "commentText": commentText,
+        "commentId": commentId,
+        "authorComment": authorComment,
+        "name": name,
+      });
+    } catch (e) {
+      // Handle errors here
+      print(e.toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final loadedContent = ModalRoute.of(context)?.settings.arguments as Content;
+    _name = loadedContent.name ?? '';
+    _fetch();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black.withOpacity(.75),
@@ -50,7 +97,8 @@ class _CommentsState extends State<Comments>{
     padding: EdgeInsets.fromLTRB(0, 10, 0, 40),
     child: Text(
     "Make a Comment: ",
-    style: TextStyle(color: Colors.black, fontSize: 24),
+    style: TextStyle(color: Colors.black, fontSize: 24
+    ),
     textAlign: TextAlign.center,
     ),
     ),
@@ -85,27 +133,23 @@ class _CommentsState extends State<Comments>{
     children: [
     ListView.builder(
     shrinkWrap: true,
-      itemCount: commentArray.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: [
-            Text(commentArray[index]),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  likes[index].isLiked = !likes[index].isLiked;
-                });
-              },
-              child: Column(
-                children: <Widget>[
-                  Icon(likes[index].isLiked ? Icons.thumb_up : Icons.thumb_up_outlined),
-                  Text(likes[index].isLiked ? 'Unlike' : 'Like')
-                ],
-              ),
-            ),
-          ],
-        );
-      },
+    itemCount: commentArray.length,
+    itemBuilder: (BuildContext context, int index) {
+    return Column(
+    children: [
+    Text('$_username: ${commentArray.elementAt(index)}'),
+    ElevatedButton(
+    onPressed: () {
+    setState(() {
+    likes[index].isLiked = !likes[index].isLiked;
+    });
+    },
+    child: Text(likes[index].isLiked ? 'Unlike' : 'Like'),
+    ),
+    ],
+    );
+    },
+    physics: ScrollPhysics()
     ),
     ],
     ),
@@ -113,7 +157,7 @@ class _CommentsState extends State<Comments>{
     ],
     ),
     ),
-        ),
+    )
     );
   }
 }
